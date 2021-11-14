9d1693b68a98c136b8988451b7d31c2204d3ce0e
Vinmar Tipay <vinmar.tipay@outlook.com>
2021-11-14 18:35:37 +0000

    Added user model and all its associated models including linker models

--- todo_db.sql
+++ todo_db.sql
@@ -1,70 +1,192 @@
-DROP DATABASE IF EXISTS `tododb`;
-CREATE DATABASE IF NOT EXISTS `tododb`;
+CREATE DATABASE  IF NOT EXISTS `tododb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `tododb`;
 
---
--- Table structure for table `todo_model`
---
-
-DROP TABLE IF EXISTS `todo_model`;
-CREATE TABLE `todo_model` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `date_created` datetime NOT NULL,
-  `date_updated` datetime NULL,
-  `date_deleted` datetime NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `body_model`
 --
 
 DROP TABLE IF EXISTS `body_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `body_model` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `value` text COLLATE utf8mb4_bin,
+  `value` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   `date_created` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `email_model`
+--
+
+DROP TABLE IF EXISTS `email_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `email_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `value_UNIQUE` (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `firstname_model`
+--
+
+DROP TABLE IF EXISTS `firstname_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `firstname_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `value_UNIQUE` (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lastname_model`
+--
+
+DROP TABLE IF EXISTS `lastname_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `lastname_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `value_UNIQUE` (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `password_model`
+--
+
+DROP TABLE IF EXISTS `password_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `password_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `value_UNIQUE` (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `pid_model`
 --
 
 DROP TABLE IF EXISTS `pid_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `pid_model` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `value` varchar(16) COLLATE utf8mb4_bin NOT NULL,
+  `value` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `date_created` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `value_UNIQUE` (`value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `title_model`
 --
 
 DROP TABLE IF EXISTS `title_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `title_model` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `value` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `value` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `date_created` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `value_UNIQUE` (`value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `todo_model`
+--
+
+DROP TABLE IF EXISTS `todo_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `todo_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  `date_deleted` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `upid_model`
+--
+
+DROP TABLE IF EXISTS `upid_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `upid_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `value_UNIQUE` (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `user_model`
+--
+
+DROP TABLE IF EXISTS `user_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `todo_body_linker_model`
 --
 
 DROP TABLE IF EXISTS `todo_body_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `todo_body_linker_model` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `date_created` datetime NOT NULL,
-  `date_updated` datetime NULL,
+  `date_updated` datetime DEFAULT NULL,
   `todo_id` bigint NOT NULL,
   `body_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -73,6 +195,7 @@ CREATE TABLE `todo_body_linker_model` (
   CONSTRAINT `todo_body_linker_model_ibfk_1` FOREIGN KEY (`body_id`) REFERENCES `body_model` (`id`),
   CONSTRAINT `todo_body_linker_model_ibfk_2` FOREIGN KEY (`todo_id`) REFERENCES `todo_model` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 
@@ -81,6 +204,8 @@ CREATE TABLE `todo_body_linker_model` (
 --
 
 DROP TABLE IF EXISTS `todo_pid_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `todo_pid_linker_model` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `date_created` datetime NOT NULL,
@@ -92,16 +217,19 @@ CREATE TABLE `todo_pid_linker_model` (
   CONSTRAINT `todo_pid_linker_model_ibfk_1` FOREIGN KEY (`pid_id`) REFERENCES `pid_model` (`id`),
   CONSTRAINT `todo_pid_linker_model_ibfk_2` FOREIGN KEY (`todo_id`) REFERENCES `todo_model` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `todo_title_linker_model`
 --
 
 DROP TABLE IF EXISTS `todo_title_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `todo_title_linker_model` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `date_created` datetime NOT NULL,
-  `date_updated` datetime NULL,
+  `date_updated` datetime DEFAULT NULL,
   `todo_id` bigint NOT NULL,
   `title_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
@@ -110,3 +238,141 @@ CREATE TABLE `todo_title_linker_model` (
   CONSTRAINT `todo_title_linker_model_ibfk_1` FOREIGN KEY (`title_id`) REFERENCES `title_model` (`id`),
   CONSTRAINT `todo_title_linker_model_ibfk_2` FOREIGN KEY (`todo_id`) REFERENCES `todo_model` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+--
+-- Table structure for table `user_email_linker_model`
+--
+
+DROP TABLE IF EXISTS `user_email_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_email_linker_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `email_id` bigint NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  UNIQUE KEY `email_id` (`email_id`),
+  CONSTRAINT `user_email_linker_model_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_model` (`id`),
+  CONSTRAINT `user_email_linker_model_ibfk_2` FOREIGN KEY (`email_id`) REFERENCES `email_model` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_firstname_linker_model`
+--
+
+DROP TABLE IF EXISTS `user_firstname_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_firstname_linker_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `firstname_id` bigint NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  KEY `firstname_id` (`firstname_id`),
+  CONSTRAINT `user_firstname_linker_model_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_model` (`id`),
+  CONSTRAINT `user_firstname_linker_model_ibfk_2` FOREIGN KEY (`firstname_id`) REFERENCES `firstname_model` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_lastname_linker_model`
+--
+
+DROP TABLE IF EXISTS `user_lastname_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_lastname_linker_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `lastname_id` bigint NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  KEY `lastname_id` (`lastname_id`),
+  CONSTRAINT `user_lastname_linker_model_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_model` (`id`),
+  CONSTRAINT `user_lastname_linker_model_ibfk_2` FOREIGN KEY (`lastname_id`) REFERENCES `lastname_model` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+--
+-- Table structure for table `user_password_linker_model`
+--
+
+DROP TABLE IF EXISTS `user_password_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_password_linker_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `password_id` bigint NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  KEY `password_id` (`password_id`),
+  CONSTRAINT `user_password_linker_model_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_model` (`id`),
+  CONSTRAINT `user_password_linker_model_ibfk_2` FOREIGN KEY (`password_id`) REFERENCES `password_model` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_todo_linker_model`
+--
+
+DROP TABLE IF EXISTS `user_todo_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_todo_linker_model` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `todo_id` bigint DEFAULT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  UNIQUE KEY `todo_id` (`todo_id`),
+  CONSTRAINT `user_todo_linker_model_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_model` (`id`),
+  CONSTRAINT `user_todo_linker_model_ibfk_2` FOREIGN KEY (`todo_id`) REFERENCES `todo_model` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_upid_linker_model`
+--
+
+DROP TABLE IF EXISTS `user_upid_linker_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_upid_linker_model` (
+  `id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `upid_id` bigint NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `upid_id_UNIQUE` (`upid_id`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  CONSTRAINT `upid_id` FOREIGN KEY (`upid_id`) REFERENCES `upid_model` (`id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user_model` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
