4f1628ebf34a23584240f236d3023511c73d5eef
Vinmar Tipay <vinmar.tipay@outlook.com>
2021-11-28 00:38:49 +0000

    Added struct types for request body for registering(sign up) and logging in a user

--- models/request_models.go
+++ models/request_models.go
@@ -4,3 +4,15 @@ type RequestBodyModel struct {
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
+
+type RequestSignUpBodyModel struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+	EmailAddress string `json:"email address"`
+	Password string `json:"password"`
+}
+
+type RequestLoginBodyModel struct {
+	EmailAddress string `json:"email address"`
+	Password string `json:"password"`
+}
